e755b80bed5d24dd299ac22283f1c8bcd3ff8e67
aremo90 <mrhamza2005@gmail.com>
2025-07-21 07:43:46 +0300

    Question 1

---
+++ Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal struct Person
+    {
+        #region attributes
+        string[] Name;
+        int[] Age;
+        int Size;
+        #endregion
+
+        public int size
+        {
+            get { return Size; }
+            set { Size = value; }
+        }
+
+        #region getters and setters
+        public string getName(int index)
+        {
+            if (index >= 0 && index < Size)
+            {
+                return Name[index];
+            }
+            else
+            {
+                Console.WriteLine("index not valid");
+                return "index is not vaild";
+            }
+        }
+        public int getAge(int index)
+        {
+            if (index >= 0 && index < Size)
+            {
+                return Age[index];
+            }
+            else
+            {
+                Console.WriteLine("index not valid");
+                return -1;
+            }
+        }
+        #endregion
+
+
+        #region constructor
+        public Person(int size)
+        {
+            Size = size;
+            Name = new string[size];
+            Age = new int[size];
+        }
+        #endregion
+        #region methods
+        public void AddPerson(int index, string name, int age)
+        {
+            if (index >= 0 || index < Size)
+            {
+                Name[index] = name;
+                Age[index] = age;
+            }
+            else
+            {
+                Console.WriteLine("index not vaild");
+            }
+        }
+
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            Person p1 = new Person(3);
+            p1.AddPerson(0, "Alice", 30);
+            p1.AddPerson(1, "Bob", 25);
+            p1.AddPerson(2, "Charlie", 35);
+
+            for (int i = 0; i < p1.size; i++)
+            {
+                Console.WriteLine($"Name: {p1.getName(i)}, Age: {p1.getAge(i)}");
+            }
+
+            #endregion
         }
     }
 }
