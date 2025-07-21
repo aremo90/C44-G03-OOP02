1525c55823cfa0d287313142eac461d73f9f51ca
aremo90 <mrhamza2005@gmail.com>
2025-07-21 07:51:59 +0300

    Question 2

---
+++ Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal struct Point
+    {
+        #region attributes
+        public int X { get; set; }
+        public int Y { get; set; }
+        #endregion
+
+        #region constructor
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        #endregion
+
+        #region methods
+        public override string ToString()
+        {
+            int difference;
+            if (X > Y)
+            {
+                difference = X - Y;
+            }
+            else
+            {
+                difference = Y - X;
+            }
+            return $"distance between: ({X} , {Y}) = {difference}";
+        }
+        #endregion
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,15 +6,27 @@
         {
             #region Q1
 
-            Person p1 = new Person(3);
-            p1.AddPerson(0, "Alice", 30);
-            p1.AddPerson(1, "Bob", 25);
-            p1.AddPerson(2, "Charlie", 35);
+            //Person p1 = new Person(3);
+            //p1.AddPerson(0, "Alice", 30);
+            //p1.AddPerson(1, "Bob", 25);
+            //p1.AddPerson(2, "Charlie", 35);
 
-            for (int i = 0; i < p1.size; i++)
-            {
-                Console.WriteLine($"Name: {p1.getName(i)}, Age: {p1.getAge(i)}");
-            }
+            //for (int i = 0; i < p1.size; i++)
+            //{
+            //    Console.WriteLine($"Name: {p1.getName(i)}, Age: {p1.getAge(i)}");
+            //}
+
+            #endregion
+
+            #region Q2
+            //Point point = new Point();
+
+            //Console.Write("enter point X: ");
+            //point.X = int.Parse(Console.ReadLine()); 
+            //Console.Write("enter point Y: ");
+            //point.Y = int.Parse(Console.ReadLine()); 
+
+            //Console.WriteLine(point.ToString());
 
             #endregion
         }
